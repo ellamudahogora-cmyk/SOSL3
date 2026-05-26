f23db1fc36c111660e0f1d6037b1a81190c78ff7
gakumba ella <ellamudahogora@gmail.com>
2026-05-26 09:42:29 +0200

    my first commit on my first project

---
+++ projectone/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import logo from './assets/sos village.png'
+</script>
+<template>
+ <img :src="logo" alt="SOS Logo" class="w-70"/>
+  <h1 class="font-bold text-center text-blue-500"> SOS Technical High School</h1>
+  <p class="italic text-center text-gray-600 mt-4">
+    SOS Technical High School is a well known technical and vocational school located in Kinyinya, Kigali, Rwanda. The school provides both general secondary education in lower secondary and technical training programs for upper level students to prepare modern careers and real world challenges.
+    <br/> <br/>
+The school is said to be promoting quality education which consist of software development and networking. Through hands on learning and technology based education, SOS Technical High School helps learners build confidence, develop professional skills, and become responsible members of society. It also creates a supportive environment where students are encouraged to discover their talents and prepare for future opportunities in higher education and employment.
+  </p>
+</template>
+
+<style scoped></style>
