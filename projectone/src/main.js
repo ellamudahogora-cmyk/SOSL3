7ff5e0beef94c49bdf9f92f81d415b48a911f33a
gakumba ella <ellamudahogora@gmail.com>
2026-05-28 16:29:23 +0200

    my second push

--- projectone/src/main.js
+++ projectone/src/main.js
@@ -1,7 +1,5 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
 import './style.css'
-
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router/index.js'
+createApp(App).use(router).mount('#app')
---
+++ projectone/src/views/Home.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+<template>
+  <!--Hero section-->
+  <section class="relative h-screen bg-cover bg-center flex items-center justify-center text-white"
+    style="background-image: url('/src/assets/rwanda7.png');"
+    >
+    <div class="bg-black/40bbg-opacity-50 absolute inset-0">
+      </div>
+      
+      <div class="relative text-center px-4">
+        <h1 class="text-5xl font-bold mb-4">convetion center, Kigali</h1>
+      </div>
+
+  </section>
+  
+
+
+  
+</template>
